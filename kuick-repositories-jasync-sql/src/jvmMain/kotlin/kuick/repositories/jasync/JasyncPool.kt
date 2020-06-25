38949f52d31f650a6dee9ee3d6244456e19abf24
Miguel Ángel Ballesteros Velasco <maballesteros@gmail.com>
2020-06-25 06:02:07 +0200

    Ajustes finales de 0.9

--- kuick-repositories-jasync-sql/src/jvmMain/kotlin/kuick/repositories/jasync/JasyncPool.kt
+++ kuick-repositories-jasync-sql/src/jvmMain/kotlin/kuick/repositories/jasync/JasyncPool.kt
@@ -3,6 +3,8 @@ package kuick.repositories.jasync
 import com.github.jasync.sql.db.QueryResult
 import com.github.jasync.sql.db.SuspendingConnection
 import com.github.jasync.sql.db.asSuspending
+import com.github.jasync.sql.db.pool.ConnectionPool
+import com.github.jasync.sql.db.postgresql.PostgreSQLConnection
 import com.github.jasync.sql.db.postgresql.PostgreSQLConnectionBuilder
 import java.util.Date
 
@@ -11,12 +13,32 @@ class JasyncPool(
     port: Int,
     database: String,
     username: String,
-    password: String
+    password: String,
+    applicationName: String = "kuik-jaync-pool",
+    maxActiveConnections: Int = 2,
+    val debug: Boolean = false
 ) {
-    private val pool =
-        PostgreSQLConnectionBuilder.createConnectionPool(
-            "jdbc:postgresql://$host:$port/$database?user=$username&password=$password"
-        )
+    private val pool: ConnectionPool<PostgreSQLConnection>
+
+    init {
+        pool = PostgreSQLConnectionBuilder.createConnectionPool {
+            this.host = host
+            this.port = port
+            this.database = database
+            this.username = username
+            this.password = password
+            this.applicationName = applicationName
+            this.maxActiveConnections = maxActiveConnections
+        }
+        log("JasyncPool ---------------------")
+        log("Created conection pool:")
+        log("  host                 : $host")
+        log("  port                 : $port")
+        log("  database             : $database")
+        log("  username             : $username")
+        log("  maxActiveConnections : $maxActiveConnections")
+        log("/JasyncPool ---------------------")
+    }
 
     fun connection(): SuspendingConnection = pool.asSuspending
 
@@ -25,14 +47,21 @@ class JasyncPool(
     suspend fun prepQuery(sql: String, values: List<Any?>): QueryResult = execute(sql, values)
 
     private suspend fun execute(sql: String, values: List<Any?>?): QueryResult {
-        // println("${Date()} [SQL] $sql ${values ?: ""}")
         val begin = System.currentTimeMillis()
         val qr = values
             ?.let { connection().sendPreparedStatement(sql, values) }
             ?: connection().sendQuery(sql)
         val end = System.currentTimeMillis()
         val lapse = (end - begin)
-        println("${Date()} [SQL] $sql ${values ?: ""} | ${qr.rowsAffected} rows, $lapse ms")
+        debug("[SQL] $sql ${values ?: ""} | ${qr.rowsAffected} rows, $lapse ms")
         return qr
+    }
+
+    private fun log(msg: String) {
+        println("${Date()} $msg")
+    }
+
+    private fun debug(msg: String) {
+        if (debug) log(msg)
     }
 }
