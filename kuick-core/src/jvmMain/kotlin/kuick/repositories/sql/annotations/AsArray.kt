00a1d49c8197c64b839c4e18d3d7b242d2458b75
Nestorferrando <ferrando.nestor2@gmail.com>
2022-06-09 08:19:37 +0200

    added comments

--- kuick-core/src/jvmMain/kotlin/kuick/repositories/sql/annotations/AsArray.kt
+++ kuick-core/src/jvmMain/kotlin/kuick/repositories/sql/annotations/AsArray.kt
@@ -1,5 +1,9 @@
 package kuick.repositories.sql.annotations
 
+/**
+Define que la lista se almacena como un Array en la BBDD y no como un string
+Afecta a la estrategia de serialización
+ **/
 
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.PROPERTY)
--- kuick-core/src/jvmMain/kotlin/kuick/repositories/sql/annotations/AsNumericId.kt
+++ kuick-core/src/jvmMain/kotlin/kuick/repositories/sql/annotations/AsNumericId.kt
@@ -1,5 +1,9 @@
 package kuick.repositories.sql.annotations
 
+/**
+Define que la variable de tipo Id se almacena como un número en la BBDD
+Afecta a la estrategia de serialización
+ **/
 
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.PROPERTY)
